456855cfb11ae65ffb21930bc40b532800bfde63
Uijin <108450394+promisingJin@users.noreply.github.com>
2025-08-03 01:09:40 +0900

    Initial commit

---
+++ src/test/java/com/jintool/spring_template/SpringTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jintool.spring_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
