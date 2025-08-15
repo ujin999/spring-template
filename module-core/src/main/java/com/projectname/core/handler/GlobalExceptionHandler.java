6931a11a503c96f6f531b672bd86bc9647ee3fb9
woojin <epahg789@gmail.com>
2025-08-15 20:38:40 +0900

    create exception handler

---
+++ module-core/src/main/java/com/projectname/core/handler/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.projectname.core.handler;
+
+import com.projectname.core.exception.ErrorCode;
+import com.projectname.core.response.ApiErrorResponse;
+import com.projectname.core.response.ApiErrorResponse.ValidationError;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException", e);
+
+        final List<ValidationError> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> new ValidationError(error.getField(), error.getDefaultMessage()))
+                .toList();
+
+        final ApiErrorResponse response = ApiErrorResponse.of(
+                ErrorCode.INVALID_INPUT_VALUE.getStatus(),
+                ErrorCode.INVALID_INPUT_VALUE.getCode(),
+                ErrorCode.INVALID_INPUT_VALUE.getMessage(),
+                errors
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ module-core/src/test/java/com/projectname/core/exception/ErrorCodeTest.java
@@ -0,0 +1,21 @@
+package com.projectname.core.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Error Code Tests")
+class ErrorCodeTest {
+    @Test
+    @DisplayName("Enum value of INVALID_INPUT_VALUE has a correct value")
+    void testInvalidInputTest() {
+        // Given
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+
+        // When & Then
+        assertThat(errorCode.getStatus()).isEqualTo(400);
+        assertThat(errorCode.getCode()).isEqualTo("C001");
+        assertThat(errorCode.getMessage()).isEqualTo("Invalid Input Value");
+    }
+}
